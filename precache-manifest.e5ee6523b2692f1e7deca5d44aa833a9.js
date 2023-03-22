9d9b97513bdf90868f2e95d66a983db088869515
ExeLxrd <tech.mccune@gmail.com>
2023-03-21 19:59:22 -0600

    Updates

---
+++ precache-manifest.e5ee6523b2692f1e7deca5d44aa833a9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b864e14dc7b382cc75c4cffc895dfb39",
+    "url": "/marketing-lp/index.html"
+  },
+  {
+    "revision": "fa70d49bdda068189820",
+    "url": "/marketing-lp/static/css/main.5e180864.chunk.css"
+  },
+  {
+    "revision": "85abe5ae30779347820b",
+    "url": "/marketing-lp/static/js/2.ccd82b22.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/marketing-lp/static/js/2.ccd82b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa70d49bdda068189820",
+    "url": "/marketing-lp/static/js/main.ccb5d594.chunk.js"
+  },
+  {
+    "revision": "a4bdf6fd106d599b7930",
+    "url": "/marketing-lp/static/js/runtime-main.350b8f6e.js"
+  },
+  {
+    "revision": "c51edfffb39a26f62006049c6974b822",
+    "url": "/marketing-lp/static/media/download__14_-removebg-preview.c51edfff.svg"
+  },
+  {
+    "revision": "4ab9430df74486bf146580eb5d8ce6b7",
+    "url": "/marketing-lp/static/media/mail.4ab9430d.svg"
+  },
+  {
+    "revision": "e30bab53542448376163ad6a37f341b1",
+    "url": "/marketing-lp/static/media/newsletter.e30bab53.svg"
+  },
+  {
+    "revision": "71f30eae279c44ee3362b43ef27f4c04",
+    "url": "/marketing-lp/static/media/privacy.71f30eae.md"
+  }
+]);
